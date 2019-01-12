d40d93ffe4143de4794ff958ec272aa210a46968
Seongjoo Jin <evanjin8@gmail.com>
2019-01-12 16:02:00 +0900

    1.13 getInitialProps

--- nomad-store/pages/index.js
+++ nomad-store/pages/index.js
@@ -1,20 +1,23 @@
 import Head from "next/head";
 import PostLink from "../components/PostLink"
 
-
-export default () => (
-		<div>
-			<Head>
-				<title>Home | nomad store</title>
-			</Head>
-			<h1>Movies:</h1>
-			<ul>
-				<li>
-					<PostLink title={"Very Important"} id={0}/>
-				</li>
-				<li>
-					<PostLink title={"Super old"} id={1}/>
-				</li>
-			</ul>
-		</div>
-);
+export default class extends React.Component {
+	render() {
+		return (
+				<div>
+					<Head>
+						<title>Home | nomad store</title>
+					</Head>
+					<h1>Movies:</h1>
+					<ul>
+						<li>
+							<PostLink title={"Very Important"} id={0}/>
+						</li>
+						<li>
+							<PostLink title={"Super old"} id={1}/>
+						</li>
+					</ul>
+				</div>
+		)
+	}
+}
--- nomad-store/pages/movie.js
+++ nomad-store/pages/movie.js
@@ -7,9 +7,16 @@ const Movie = props => (
 				<title>{props.router.query.title} | Nomad Store</title>
 			</Head>
 			{JSON.stringify(props)}
-			<h1>{props.router.query.title}</h1>
+			<h1>{props.title}</h1>
 			<p>lalala</p>
 		</div>
 );
+
+Movie.getInitialProps = async()=>{
+	// call the api
+	// parse the result
+	// return result
+	return {movie: "lololo"}
+};
 
  export default withRouter(Movie);
