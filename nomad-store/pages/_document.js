b9766aaec2d445cba7addddc221cb71c23a52f5f
Seongjoo Jin <evanjin8@gmail.com>
2019-01-17 09:01:12 +0900

    3.3 Installing Ant Design

--- nomad-store/pages/_document.js
+++ nomad-store/pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document{
 				<html>
 					<Head>
 						<meta name="author" content={"Evan Jin"} />
+						<link href="//cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.css" rel="stylesheet" />
 					</Head>
 					<body>
 						<Main />
